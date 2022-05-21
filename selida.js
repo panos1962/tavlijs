9c2e59fd5cbbb16a15b8a00e6653f3112c68713f
panos1962 <panos1962@gmail.com>
2022-05-21 09:43:09 +0300

    modifications

--- selida.js
+++ selida.js
@@ -4,6 +4,6 @@ $(function() {
 	let bodyDOM = $(document.body);
 	let t1 = new tavlijs.tavli();
 
-	t1.domCreate();
-	t1.dom.appendTo(bodyDOM);
+	t1.domGet().appendTo(bodyDOM);
+	t1.apikonisi();
 });
--- tavlijs.js
+++ tavlijs.js
@@ -3,33 +3,40 @@
 var tavlijs = {};
 
 tavlijs.platosDefault = 600;
+tavlijs.ipsosAnalogia = 0.9;
+
+tavlijs.thesiXroma = [
+	'#D2B48C',
+	'#CD853E'
+];
 
 ///////////////////////////////////////////////////////////////////////////////@
 
 tavlijs.tavli = function(props) {
+	let i;
+
 	if (props === undefined)
 	props = {};
 
-	for (let i in props)
-	this[i] = props[i];
-
 	if (!props.hasOwnProperty('platos'))
-	this.platos = tavlijs.platosDefault;
+	props.platos = tavlijs.platosDefault;
+
+	for (i in props)
+	this[i] = props[i];
 
 	this.thiki = [];
 
-	for (let pektis = 0; pektis < 2; pektis++)
-	this.thiki[pektis] = new tavlijs.thiki(pektis);
+	for (i = 0; i < 2; i++) {
+		this.thiki[i] = new tavlijs.thiki(i);
+
+		for (let j = 0; j < 15; j++)
+		this.thiki[i].plist.pouliPush(new tavlijs.pouli(i, j));
+	}
 
 	this.exo = [];
 
-	for (let pektis = 0; pektis < 2; pektis++)
-	this.exo[pektis] = new tavlijs.exo(pektis);
-
-	for (let pektis = 0; pektis < 2; pektis++) {
-		for (let i = 0; i < 15; i++)
-		this.thiki[pektis][i] = new tavlijs.pouli(pektis, i);
-	}
+	for (i = 0; i < 2; i++)
+	this.exo[i] = new tavlijs.exo(i);
 };
 
 tavlijs.tavli.prototype.domCreate = function() {
@@ -37,50 +44,135 @@ tavlijs.tavli.prototype.domCreate = function() {
 	addClass('tavlijsTavli').
 	css({
 		"width": this.platos + 'px',
-		"height": (this.platos * 1.0) + 'px'
+		"height": (this.platos * tavlijs.ipsosAnalogia) + 'px'
 	});
 
 	for (let i = 0; i < 2; i++)
 	this.thiki[i].domGet().appendTo(this.dom);
 
 	for (let i = 0; i < 2; i++)
-	$('<div>').
-	addClass('tavlijsExo').
-	addClass('tavlijsExo' + i).
-	appendTo(this.dom);
+	this.exo[i].domGet().appendTo(this.dom);
 
 	for (let i = 0; i < 4; i++)
-	$('<div>').
-	addClass('tavlijsPerioxi').
-	addClass('tavlijsPerioxi' + i).
-	appendTo(this.dom);
+	(new tavlijs.perioxi(i)).domGet().appendTo(this.dom);
 
 	for (let i = 0; i < 2; i++)
-	$('<div>').
-	addClass('tavlijsZaria').
-	addClass('tavlijsZaria' + i).
-	appendTo(this.dom);
+	(new tavlijs.zaria(i)).domGet().appendTo(this.dom);
+
+	return this;
+};
+
+tavlijs.tavli.prototype.domGet = function() {
+	if (!this.hasOwnProperty('dom'))
+	this.domCreate();
+
+	return this.dom;
+};
+
+tavlijs.tavli.prototype.apikonisi = function() {
+	$('.tavlijsThesi').each(function() {
+		let id = $(this).data('id');
+
+		let dom = $('<canvas>').
+		addClass('tavlijsThesiTrigono').
+		appendTo($(this));
+
+		let w = dom.width();
+		let h = dom.height();
+
+		dom.
+		attr({
+			'width': w,
+			'height': h
+		});
+
+		let ctx = dom[0].getContext('2d');
+
+		ctx.beginPath();
+		ctx.moveTo(0, h);
+		ctx.lineTo(w / 2, h * 0.03);
+		ctx.lineTo(w, h);
+		ctx.closePath();
+
+		ctx.fillStyle = tavlijs.thesiXroma[id % 2];
+		ctx.fill();
+	});
 
 	return this;
 };
 
 ///////////////////////////////////////////////////////////////////////////////@
 
+tavlijs.perioxi = function(id) {
+	this.id = id;
+}
+
+tavlijs.perioxi.prototype.domCreate = function() {
+	let i;
+
+	let perioxiDom = $('<div>').
+	addClass('tavlijsPerioxi');
+
+	for (i = 0; i < 6; i++)
+	$('<div>').
+	data('id', i).
+	addClass('tavlijsThesi').
+	appendTo(perioxiDom);
+
+	this.dom = $('<div>').
+	data('id', this.id).
+	addClass('tavlijsPerioxiArea').
+	addClass('tavlijsPerioxiArea' + this.id).
+	append(perioxiDom);
+
+	return this;
+};
+
+tavlijs.perioxi.prototype.domGet = function() {
+	if (!this.hasOwnProperty('dom'))
+	this.domCreate();
+
+	return this.dom;
+};
+
+///////////////////////////////////////////////////////////////////////////////@
+
+tavlijs.zaria = function(pektis) {
+	this.pektis = pektis;
+}
+
+tavlijs.zaria.prototype.domCreate = function() {
+	this.dom = $('<div>').
+	data('id', this.pektis).
+	addClass('tavlijsZariaArea').
+	addClass('tavlijsZariaArea' + this.pektis).
+	append($('<div>').
+	addClass('tavlijsZaria'));
+
+	return this;
+};
+
+tavlijs.zaria.prototype.domGet = function() {
+	if (!this.hasOwnProperty('dom'))
+	this.domCreate();
+
+	return this.dom;
+};
+
+///////////////////////////////////////////////////////////////////////////////@
+
 tavlijs.thiki = function(pektis) {
 	this.pektis = pektis;
-	this.plist = [];
-
-	for (let i = 0; i < 15; i++)
-	this.plist[i] = new tavlijs.pouli(pektis, i);
+	this.plist = new tavlijs.dana();
 };
 
 tavlijs.thiki.prototype.domCreate = function() {
 	this.dom = $('<div>').
 	data('pektis', this.pektis).
-	addClass('thiki').
-	addClass('thiki' + this.pektis);
+	addClass('tavlijsThiki').
+	addClass('tavlijsThiki' + this.pektis);
 
-	for (let i = 0; i < 15; i++)
+	for (let i = 0; i < this.plist.length; i++)
 	this.plist[i].domGet().appendTo(this.dom);
 
 	return this;
@@ -97,22 +189,53 @@ tavlijs.thiki.prototype.domGet = function() {
 
 tavlijs.exo = function(pektis) {
 	this.pektis = pektis;
-	this.plist = [];
+	this.plist = new tavlijs.dana();
 };
 
 tavlijs.exo.prototype.domCreate = function() {
 	this.dom = $('<div>').
 	data('pektis', this.pektis).
-	addClass('exo').
-	addClass('exo' + this.pektis);
+	addClass('tavlijsExo').
+	addClass('tavlijsExo' + this.pektis);
 
-	for (let i = 0; i < 15; i++)
+	for (let i = 0; i < this.plist.length; i++)
 	this.plist[i].domGet().appendTo(this.dom);
 
 	return this;
 };
 
-tavlijs.thiki.prototype.domGet = function() {
+tavlijs.exo.prototype.domGet = function() {
+	if (!this.hasOwnProperty('dom'))
+	this.domCreate();
+
+	return this.dom;
+};
+
+///////////////////////////////////////////////////////////////////////////////@
+
+tavlijs.dana = function() {
+	this.plist = [];
+}
+
+tavlijs.dana.prototype.pouliPush = function(pouli) {
+	this.plist.push(pouli);
+	return this;
+};
+
+tavlijs.dana.prototype.pouliPop = function() {
+	return this.plist.pop();
+};
+
+tavlijs.dana.prototype.domCreate = function() {
+	this.dom = $('<div>').addClass('tavlijsDana');
+
+	for (let i = 0; i < this.plist.length; i++)
+	this.dom.prepend(this.plist[i].domGet());
+
+	return this;
+};
+
+tavlijs.dana.prototype.domGet = function() {
 	if (!this.hasOwnProperty('dom'))
 	this.domCreate();
 
@@ -141,3 +264,5 @@ tavlijs.pouli.prototype.domGet = function() {
 
 	return this.dom;
 };
+
+///////////////////////////////////////////////////////////////////////////////@
