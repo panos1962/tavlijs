55a5cf9f154af914a9eee577e46f517a4b0c6878
panos1962 <panos1962@gmail.com>
2022-05-22 10:28:34 +0300

    modifications

--- selida.js
+++ selida.js
@@ -5,28 +5,40 @@ $(function() {
 	let t;
 
 	[
-		70,
-		100,
-		200,
-		300,
-		400,
-		450,
-		500,
-		700,
+70,
+/*
+100,
+*/
+200,
+/*
+300,
+400,
+*/
+/*
+450,
+*/
+/*
+500,
+*/
+700,
+/*
+2000,
+*/
+3000,
 	].forEach(function(w) {
 		t = new tavlijs.tavli({'platos': w});
 
 for (let i = 0; i < 15; i++)
-t.thiki[0].pouliPush(new tavlijs.pouli(0, i));
+t.thiki[0].pouliPush(new tavlijs.pouli(t, 0, i));
 
 for (let i = 0; i < 15; i++)
-t.thiki[1].pouliPush(new tavlijs.pouli(1, i));
+t.thiki[1].pouliPush(new tavlijs.pouli(t, 1, i));
 
 for (let i = 0; i < 5; i++)
-t.exo[0].pouliPush(new tavlijs.pouli(0, i));
+t.exo[0].pouliPush(new tavlijs.pouli(t, 0, i));
 
 for (let i = 0; i < 5; i++)
-t.exo[1].pouliPush(new tavlijs.pouli(1, i));
+t.exo[1].pouliPush(new tavlijs.pouli(t, 1, i));
 
 for (let i = 0; i < 5; i++)
 t.thesi[0].pouliPush(new tavlijs.pouli(0, i));
--- tavlijs.js
+++ tavlijs.js
@@ -2,15 +2,48 @@
 
 var tavlijs = {};
 
+// Κάθε ταμπλό έχει ένα συνολικό πλάτος στο οποίο, εκτός από το κυρίως ταμπλό
+// που παίζεται το παιχνίδι, περιλαμβάνονται και οι πλαϊνές ντάνες (αριστερά
+// τα μαζεμένα πούλια και δεξιά τα χτυπημένα πούλια). Το συνολικό πλάτος του
+// ταμπλό μετριέται σε pixel αποτελεί property του ταμπλό. Αν δεν καθοριστεί
+// παίρνει τιμή που καθορίζουμε εδώ με τη σταθερά "platosDefault".
+
 tavlijs.platosDefault = 100;
+
+// Το συνολικό ύψος του ταμπλό προκύπτει κατ' αναλογία από το πλάτος. Με άλλα
+// λόγια η αναλογία ύψους/πλάτους είναι σταθερή και καθορίζεται από την τιμή
+// της σταθεράς "ipsosAnalogia".
+
 tavlijs.ipsosAnalogia = 0.9;
 
+// Ακολουθούν τα χρώματα που παίρνουν εναλλάξ οι θέσεις στο ταμπλό. Θυμίζουμε
+// ότι οι 24 θέσεις αριθμούνται από 0 έως 23, εκκινούν από τη θέση 0 κάτω
+// αριστερά και καταλήγουν στη θέση 23 επάνω αριστερά, ακολουθώντας κυκλική
+// τροχιά με φορά αντίστροφη από αυτήν των δεικτών του ωρολογίου.
+
 tavlijs.thesiXroma = [
-	'#D2B48C',
-	'#CD853E'
+	'#D2B48C',	// 0, 2, 4,... 22
+	'#CD853E'	// 1, 3, 5,... 23
+];
+
+tavlijs.pouliPlatosAnalogia = 0.068;
+tavlijs.pouliIpsosAnalogia = 0.38;
+
+tavlijs.pouliMesaXroma = [
+	'#F5F5DC',
+	'#1D3025'
+];
+
+tavlijs.pouliPerigramaXroma = [
+	'#003C00',
+	'#325D1E'
 ];
 
 ///////////////////////////////////////////////////////////////////////////////@
+
+// Ακολουθούν οι προδιαγραφές που καθορίζουν τα αντικείμενα τύπου "tavli" που
+// περιλαμβάνουν τις πλαϊνές ντάνες, το ταμπλό αυτό καθαυτό, τις 24 θέσεις του
+// ταμπλό και τα πούλια.
 
 tavlijs.tavli = function(props) {
 	let i;
@@ -24,17 +57,37 @@ tavlijs.tavli = function(props) {
 	if (!this.hasOwnProperty('platos'))
 	props['platos'] = tavlijs.platosDefault;
 
+	// Ως «θήκες» ορίζουμε τις ντάνες που βρίσκονται έξω από το ταμπλό,
+	// στην αριστερά πλευρά. Πρόκειται για δύο ντάνες όπου στην κάτω
+	// ντάνα τοποθετούνται τα πούλια του παίκτη 0 και στην πάνω ντάνα
+	// τοποθετούνται τα πούλια του παίκτη 1. Στις θήκες τοποθετούνται
+	// αρχικά τα 15 πούλια, πριν αρχίσει οποιοδήποτε παιχνίδι, ενώ τα
+	// πούλια επανατοποθετούνται στις θήκες στη φάση του μαζέματος. Οι
+	// δύο θήκες ορίζονται ως ένα array δύο θέσεων με τη θήκη 0 να
+	// αφορά τα πούλια του παίκτη 0, και τη θήκη 1 να αφορά τα πούλια
+	// του παίκτη 1.
+
 	if (!this.hasOwnProperty('thiki'))
 	this.thiki = [];
 
 	for (i = 0; i < 2; i++)
 	this.thiki[i] = new tavlijs.thiki(this, i);
 
+	// Ως «έξω» ορίζουμε τις ντάνες που βρίσκονται έω από το ταμπλό, στη
+	// δεξιά πλευρά, και αφορούν στα χτυπημένα πούλια που παρουσιάζονται
+	// στο παιχνίδι «πόρτες». Κατά τα λοιπά οι «έξω» ντάνες μοιάζουν με
+	// τις «θήκες» όσον αφορά τη χωροταξία: κάτω αριστερά τοποθετουνται
+	// τα χτυπημένα πούλια του παίκτη 0, ενώ επάνω αριστερά τοποθετούνται
+	// τα χτυπημένα πούλια του παίκτη 1.
+
 	if (!this.hasOwnProperty('exo'))
 	this.exo = [];
 
 	for (i = 0; i < 2; i++)
 	this.exo[i] = new tavlijs.exo(this, i);
+
+	// Ακολουθεί το array 24 θέσεων, που περιλαμβάνει όλες τις θέσεις
+	// στις οποίες μπορούν να τοποθετηθούν πούλια μέσα στο ταμπλό.
 
 	if (!this.hasOwnProperty('thesi'))
 	this.thesi = [];
@@ -117,6 +170,7 @@ tavlijs.tavli.prototype.apikonisi = function() {
 		ctx.fill();
 	});
 
+/*
 	dom.find('.tavlijsPouliDana').each(function() {
 		let w = $(this).width();
 		let css = {
@@ -146,6 +200,7 @@ tavlijs.tavli.prototype.apikonisi = function() {
 
 		$(this).css(css);
 	});
+*/
 
 	return this;
 };
@@ -356,15 +411,80 @@ tavlijs.dana.prototype.domGet = function() {
 
 ///////////////////////////////////////////////////////////////////////////////@
 
-tavlijs.pouli = function(pektis, id) {
+tavlijs.pouli = function(tavli, pektis, id) {
+	this.tavli = tavli;
 	this.pektis = pektis;
 	this.id = id;
 };
 
 tavlijs.pouli.prototype.domCreate = function() {
+	let w = this.tavli.platos * tavlijs.pouliPlatosAnalogia;
+	let h = w * tavlijs.pouliIpsosAnalogia;
+
+	let canvasDom = $('<canvas>').
+	attr({
+		'width': w,
+		'height': h
+	});
+
+	let ctx = canvasDom[0].getContext('2d');
+
+	ctx.beginPath();
+	ctx.moveTo(0, 0);
+	ctx.lineTo(w, 0);
+	ctx.lineTo(w, h);
+	ctx.closePath();
+
+	ctx.fillStyle = tavlijs.pouliMesaXroma[this.pektis];
+	ctx.fill();
+
 	this.dom = $('<div>').
+	data('tavli', this.tavli).
 	data('pektis', this.pektis).
-	data('id', this.id);
+	data('id', this.id).
+	addClass('tavlijsPouli').
+	addClass('tavlijsPouli' + this.pektis).
+	append(canvasDom);
+
+	canvasDom = $('<canvas>').
+	attr({
+		'width': w,
+		'height': h
+	});
+
+	let r = h * 0.1;
+	let dh = h * 0.05;
+	let dw = w * 0.03;
+	let lw = h * 0.15;
+
+	ctx = canvasDom[0].getContext('2d');
+
+	ctx.beginPath();
+	ctx.moveTo(r + dw + lw, dh + lw);
+	ctx.lineTo(w - r - dw - lw, dh + lw);
+	ctx.quadraticCurveTo(w - dw - lw, dh + lw, w - dw - lw, dh + r + lw);
+	ctx.lineTo(w - dw - lw, h - dh - r - lw);
+	ctx.quadraticCurveTo(w - dw - lw, h - dh - lw, w - r - dw - lw, h - dh - lw);
+	ctx.lineTo(r + dw + lw, h - dh - lw);
+	ctx.quadraticCurveTo(dw + lw, h - dh - lw, dw + lw, h - r - dh - lw);
+	ctx.lineTo(dw + lw, dh + r + lw);
+	ctx.quadraticCurveTo(dw + lw, dh + lw, r + dw + lw, dh + lw);
+	ctx.closePath();
+
+	ctx.lineWidth = lw * 2;
+	ctx.strokeStyle = tavlijs.pouliPerigramaXroma[this.pektis];
+	ctx.stroke();
+
+	ctx.fillStyle = tavlijs.pouliMesaXroma[this.pektis];
+	ctx.fill();
+
+	this.domDana = $('<div>').
+	data('tavli', this.tavli).
+	data('pektis', this.pektis).
+	data('id', this.id).
+	addClass('tavlijsPouliDana').
+	addClass('tavlijsPouliDana' + this.pektis).
+	append(canvasDom);
 
 	return this;
 };
@@ -377,7 +497,10 @@ tavlijs.pouli.prototype.domGet = function() {
 };
 
 tavlijs.pouli.prototype.domDanaGet = function() {
-	let dom = this.domGet();
+	if (!this.hasOwnProperty('domDana'))
+	this.domCreate();
+
+	let dom = this.domDana;
 
 	dom.
 	removeClass().
